61783d5e9c6b5c75adfcb43b3cb034315f973720
LEEW <lee>
2016-09-09 22:44:47 +0800

    add num

--- app/src/main/java/com/news/leew/gitdemo/MainActivity.java
+++ app/src/main/java/com/news/leew/gitdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private static final int mData = 123;
+	private static final int mList = 456;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
